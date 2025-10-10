2c5e60d2bb52e7e1c7ee9ca5deda76cb4b7b528f
yvanddniyo <nyvan80076@gmail.com>
2025-10-10 20:56:18 +0200

    Setting up database

---
+++ database/mongoose.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const MONGO_URL = process.env.MONGODB_URL
+
+declare global {
+  var mongooseCache : {
+    conn: typeof mongoose | null
+    promise: Promise<typeof mongoose> | null
+  }
+}
+
+let cached = global.mongooseCache;
+
+if(!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null};
+}
+
+export const connectToDatabase = async() => {
+  if (!MONGO_URL) throw new Error("MONGODB_URL must be set with .env")
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URL, { bufferCommands: false })
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+ console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGO_URL}`)
+}
